ef76947f9025c70df2b6ce7c26ac7c6e9f470b13
Thiago Maciel <thiagomrvieira@gmail.com>
2019-05-18 21:58:39 -0300

    Utilizando o belongsTo

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,41 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Produto;
+use App\Categoria;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/categorias', function () {
+    $cat = Categoria::all();
+    if (count($cat)==0){
+        echo "<h4>Sem categorias cadastradas</h4>";
+    }else{ 
+        foreach ($cat as $c) {
+            echo "<p>" . $c->id . " - " .$c->nome . "</p>";
+        }
+    }    
+});
+
+
+
+Route::get('/produtos', function () {
+    $prod = Produto::all();
+    if (count($prod)==0){
+        echo "<h4>Sem produtos cadastrados</h4>";
+    }else{ 
+        
+            
+            echo "<table>";
+                echo "<thead><tr><td>Nome</td><td>Estoque</td><td>Preço</td><td>Categoria</td></tr></thead> ";
+                foreach ($prod as $p) {
+                    echo "<tr>";
+                        echo "<td>" . $p->nome . "</td>";
+                        echo "<td>" . $p->estoque . "</td>";
+                        echo "<td>" . $p->preco . "</td>";
+                        echo "<td>" . $p->categoria->nome . "</td>";
+                    echo "</tr>";
+                }
+               
+                
+            echo "</table>";
+        
+    }    
 });
